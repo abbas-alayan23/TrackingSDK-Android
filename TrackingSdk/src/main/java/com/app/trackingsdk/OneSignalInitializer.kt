e3da30b63ea93a572ca8038eb86f5730a2955c1f
Abbas <abbas.alayan22@gmail.com>
2024-10-28 14:23:03 +0200

    Initial commit

---
+++ TrackingSdk/src/main/java/com/app/trackingsdk/OneSignalInitializer.kt
@@ -0,0 +1,28 @@
+package com.app.trackingsdk
+
+import android.content.Context
+import android.util.Log
+
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+object OneSignalInitializer {
+
+    fun initialize(context: Context, oneSignalKey: String) {
+        OneSignal.Debug.logLevel = LogLevel.DEBUG
+        if (oneSignalKey.isBlank()) {
+            Log.e("OneSignalInitializer", "OneSignal key is blank or missing")
+            return
+        }
+        OneSignal.initWithContext(context, oneSignalKey)
+
+
+        CoroutineScope(Dispatchers.IO).launch {
+            OneSignal.Notifications.requestPermission(false)
+        }
+    }
+
+}
